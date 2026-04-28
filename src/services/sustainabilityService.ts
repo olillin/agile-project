f2a6c87d6ecd4c2297912578259699dc9ba59c24
ASmaller <linus@ordal.se>
2026-04-28 23:04:23 +0200

    Imported .env.development to the tests

--- src/services/sustainabilityService.ts
+++ src/services/sustainabilityService.ts
@@ -3,23 +3,23 @@
 const api_url = process.env.GREEN_BITE_API_URL || "http://localhost:80/";
 
 type Ingredient = {
-  name: string;
-  amount: number;
-  unit: string;
+    name: string;
+    amount: number;
+    unit: string;
 };
 
 export async function eco_score(ingredients: Ingredient[]) {
-  let call = api_url + "score?";
+    let call = api_url + "/score?";
 
-  call += ingredients
-    .map(
-      ({ amount, unit, name }) =>
-        `ingredients=${encodeURIComponent(`${amount} ${unit} of ${name}`)}`
-    )
-    .join("&");
+    call += ingredients
+        .map(
+            ({ amount, unit, name }) =>
+                `ingredients=${encodeURIComponent(`${amount} ${unit} of ${name}`)}`
+        )
+        .join("&");
 
-  const data = await fetch(call, {
-    method: "POST",
-  });
-  return data.json();
+    const data = await fetch(call, {
+        method: "POST",
+    });
+    return data.json();
 }
