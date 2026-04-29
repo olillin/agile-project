a0cdb7ea0f75b16874a958d5219c8deab07e2759
Daniel Kindvall <daniel.kindvall05@gmail.com>
2026-04-29 16:08:55 +0200

    add addReview function

---
+++ src/services/reviewService.ts
@@ -0,0 +1,19 @@
+import { Ingredient, Lunch, Prisma, Review } from "@/generated/prisma/client";
+import { prisma } from "@/lib/prisma";
+
+
+export async function addReview(rating: number, lunchId: number, user?: number, comment?: string) {
+
+    let currentDate = new Date(Date.now())
+
+    const review = prisma.review.create({
+        data: {
+            rating: rating,
+            comment: comment,
+            date: currentDate,
+            lunchId: lunchId,
+            userId: user
+        }
+    })
+
+}
