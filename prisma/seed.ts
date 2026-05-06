76e471c7e0fb64aa7c79a41fdc0e06a1352e155b
ASmaller <linus@ordal.se>
2026-05-06 10:26:36 +0200

    fixed formatting

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,69 +8,68 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 async function main() {
-    const meatballsContent = {
-        name: "Meatballs",
-        eco_score: 1,
-        ingredients: {
-            create: [
-                { name: "Meat", amount: 100, unit: "g" },
-                { name: "Potatoes", amount: 200, unit: "g" },
-                { name: "Gravy", amount: 150, unit: "ml" },
-            ],
+  const meatballsContent = {
+    name: "Meatballs",
+    eco_score: 1,
+    ingredients: {
+      create: [
+        { name: "Meat", amount: 100, unit: "g" },
+        { name: "Potatoes", amount: 200, unit: "g" },
+        { name: "Gravy", amount: 150, unit: "ml" },
+      ],
+    },
+    reviews: {
+      create: [
+        {
+          rating: 5,
+          comment: "Delicious!",
+          date: new Date(),
+          userId: null,
         },
-        reviews: {
-            create: [
-                {
-                    rating: 5,
-                    comment: "Delicious!",
-                    date: new Date(),
-                    userId: null,
-                },
-            ],
+      ],
+    },
+    servings: [new Date("2024-01-01"), new Date("2024-01-02")],
+  };
+  const meatballs = await prisma.lunch.upsert({
+    where: { name: meatballsContent.name },
+    update: meatballsContent,
+    create: meatballsContent,
+  });
+  const tacosContent = {
+    name: "Tacos",
+    eco_score: 1,
+    ingredients: {
+      create: [
+        { name: "Meat", amount: 100, unit: "g" },
+        { name: "Corn", amount: 200, unit: "g" },
+        { name: "Salsa", amount: 150, unit: "ml" },
+      ],
+    },
+    reviews: {
+      create: [
+        {
+          rating: 5,
+          comment: "Delicious!",
+          date: new Date(),
+          userId: null,
         },
-        servings: [new Date("2024-01-01"), new Date("2024-01-02")],
-    }
-    const meatballs = await prisma.lunch.upsert({
-        where: { name: meatballsContent.name },
-        update: meatballsContent,
-        create: meatballsContent,
-    });
-    const tacosContent = {
-
-        name: "Tacos",
-        eco_score: 1,
-        ingredients: {
-            create: [
-                { name: "Meat", amount: 100, unit: "g" },
-                { name: "Corn", amount: 200, unit: "g" },
-                { name: "Salsa", amount: 150, unit: "ml" },
-            ],
-        },
-        reviews: {
-            create: [
-                {
-                    rating: 5,
-                    comment: "Delicious!",
-                    date: new Date(),
-                    userId: null,
-                },
-            ],
-        },
-        servings: [new Date("2024-01-01"), new Date("2024-01-02")],
-    }
-    const tacos = await prisma.lunch.upsert({
-        where: { name: tacosContent.name },
-        update: tacosContent,
-        create: tacosContent,
-    });
-    console.log({ meatballs, tacos });
+      ],
+    },
+    servings: [new Date("2024-01-01"), new Date("2024-01-02")],
+  };
+  const tacos = await prisma.lunch.upsert({
+    where: { name: tacosContent.name },
+    update: tacosContent,
+    create: tacosContent,
+  });
+  console.log({ meatballs, tacos });
 }
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async e => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/services/lunchService.ts
+++ src/services/lunchService.ts
@@ -5,95 +5,95 @@ import { prisma } from "@/lib/prisma";
 import { Day } from "@/lib/types";
 
 export async function getAll() {
-    return await prisma.lunch.findMany();
+  return await prisma.lunch.findMany();
 }
 
 export async function getLunch(
-    identifier: number | string
+  identifier: number | string
 ): Promise<Lunch | Lunch[] | null> {
-    if (typeof identifier === "number") {
-        return await prisma.lunch.findUnique({
-            where: { id: identifier },
-        });
-    }
-    if (typeof identifier === "string") {
-        return await prisma.lunch.findUnique({
-            where: { name: identifier },
-        });
-    }
-    return null;
+  if (typeof identifier === "number") {
+    return await prisma.lunch.findUnique({
+      where: { id: identifier },
+    });
+  }
+  if (typeof identifier === "string") {
+    return await prisma.lunch.findUnique({
+      where: { name: identifier },
+    });
+  }
+  return null;
 }
 
 export async function getLunchesByDate(date: Date): Promise<Lunch[]> {
-    return await prisma.lunch.findMany({
-        where: { servings: { has: date } },
-    });
+  return await prisma.lunch.findMany({
+    where: { servings: { has: date } },
+  });
 }
 
 export async function deleteLunch(id: number) {
-    return await prisma.lunch.delete({
-        where: { id },
-    });
+  return await prisma.lunch.delete({
+    where: { id },
+  });
 }
 
 export async function addlunch(name: string, ingredients: Ingredient[]) {
-    const lunch = await prisma.lunch.create({
-        data: {
-            name: name,
-            eco_score: NaN,
-            ingredients: {
-                create: ingredients.map(ingredients => ({
-                    name: ingredients.name,
-                    unit: ingredients.unit,
-                    amount: ingredients.amount,
-                })),
-            },
-            reviews: {
-                create: [],
-            },
-        },
-    });
-    return lunch;
+  const lunch = await prisma.lunch.create({
+    data: {
+      name: name,
+      eco_score: NaN,
+      ingredients: {
+        create: ingredients.map(ingredients => ({
+          name: ingredients.name,
+          unit: ingredients.unit,
+          amount: ingredients.amount,
+        })),
+      },
+      reviews: {
+        create: [],
+      },
+    },
+  });
+  return lunch;
 }
 
 export async function getDays(): Promise<Day[]> {
-    const dayMap = new Map<Date, Lunch[]>();
-    const lunches = await getAll();
+  const dayMap = new Map<Date, Lunch[]>();
+  const lunches = await getAll();
 
-    lunches.forEach(lunch => {
-        lunch.servings.forEach(date => {
-            const ls = dayMap.get(date);
-            if (ls == undefined) {
-                dayMap.set(date, [lunch]);
-            } else {
-                ls.push(lunch);
-                dayMap.set(date, ls);
-            }
-        });
+  lunches.forEach(lunch => {
+    lunch.servings.forEach(date => {
+      const ls = dayMap.get(date);
+      if (ls == undefined) {
+        dayMap.set(date, [lunch]);
+      } else {
+        ls.push(lunch);
+        dayMap.set(date, ls);
+      }
     });
+  });
 
-    const days: Day[] = [];
-    dayMap.forEach((options, date) => {
-        days.push({
-            id: "a",
-            label: "b",
-            date: date.toString(),
-            isToday: true,
-            options: options.map(lunch => {
-                return {
-                    id: lunch.id.toString(),
-                    line: "Vegetarian",
-                    name: lunch.name,
-                    color: "#C87865",
-                    pattern: 45,
-                    tags: [],
-                    climate: lunch.eco_score,
-                    climateLabel: "low",
-                    desc: "f",
-                };
-            }),
-        });
+  const days: Day[] = [];
+  dayMap.forEach((options, date) => {
+    days.push({
+      id: "a",
+      label: "b",
+      date: date.toString(),
+      isToday: true,
+      options: options.map(lunch => {
+        return {
+          id: lunch.id.toString(),
+          line: "Vegetarian",
+          name: lunch.name,
+          color: "#C87865",
+          pattern: 45,
+          tags: [],
+          climate: lunch.eco_score,
+          climateLabel: "low",
+          desc: "f",
+        };
+      }),
     });
+  });
 
-    return days;
+  return days;
 }
