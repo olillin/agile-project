0faf5c29492953e8afcc23bc41bb3cb81e6dc049
Oli <oli@olillin.com>
2026-05-14 11:16:16 +0200

    Add user id to session

--- src/app/callback/route.ts
+++ src/app/callback/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 // Mocked callback route. Creates a new session without authentication.
 export async function GET(req: NextRequest) {
   // Create a new session
-  await createSession("Testu", "Exempelsson");
+  await createSession(1, "Testu", "Exempelsson");
 
   // Redirect to the admin dashboard
   return NextResponse.redirect(new URL("/admin", req.url));
